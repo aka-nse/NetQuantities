70baecaf5e45e4b83a2987d55dcc51a23962c0d6
unknown <nakase.d1220@gmail.com>
2023-12-31 14:51:59 +0900

    arrange halfway quantities

--- src/NetQuantities.Sample/Samples/QuantityInfos.cs
+++ src/NetQuantities.Sample/Samples/QuantityInfos.cs
@@ -21,24 +21,13 @@ internal class QuantityInfos : IUsageSample
             .Where(t => t.IsValueType
                 && !t.IsGenericType
                 && t.GetCustomAttributes().Any(x => x.GetType().Name == "QuantityAttribute"));
-        var infos = new List<QuantityInfo>();
         foreach (var type in types)
         {
             // for reflection of ref struct, explicitly named backing field is provided.
             var info = type.GetField(nameof(QDimensionless._Info), flags)!.GetValue(null) as QuantityInfo;
-            infos.Add(info!);
 
-            // stdout.WriteLine($"{type.Name}: {info}");
+            stdout.WriteLine($"{info.Name, -24} (Dimension={info.Dimension})");
         }
-
-        void isConflict(int L, int M, int T, int I, int Th, int N, int J)
-        {
-            var has = infos!.Any(x => x.Dimension == new QuantityInfo.DimensionInfo(L, M, T, I, Th, N, J));
-            stdout.WriteLine($"{has,5} <= L :{L,2}, M :{M,2}, T :{T,2}, I :{I,2}, Th:{Th,2}, N :{N,2}, J :{J,2}");
-        }
-
-        isConflict(L: 1, M: 1, T: 0, I: 0, Th: 0, N: 0, J: 0);
-        isConflict(L: 1, M: 0, T: 1, I: 0, Th: 0, N: 0, J: 0);
     }
 
 }
--- src/NetQuantities/QHalfway_NoNamedDimensions.g.cs
+++ src/NetQuantities/QHalfway_NoNamedDimensions.g.cs
@@ -9,6 +9,30 @@ namespace NetQuantities;
 
 
 /// <summary>
+/// Calculation halfway type that represents [L^-2] dimension.
+/// This type is intended to be used only during conversion to other types,
+/// therefore it is defined as ref struct type.
+/// </summary>
+[Quantity(L: -2, M: 0, T: 0, I: 0, Th: 0, N: 0, J: 0)]
+[QuantityOperation(typeof(QHalfway_Lm2_M0_T0_I0_Th0_N0_J0), typeof(QLength), typeof(QSpatialFrequency))]
+public readonly ref partial struct QHalfway_Lm2_M0_T0_I0_Th0_N0_J0
+{
+}
+
+
+/// <summary>
+/// Calculation halfway type that represents [M^-1] dimension.
+/// This type is intended to be used only during conversion to other types,
+/// therefore it is defined as ref struct type.
+/// </summary>
+[Quantity(L: 0, M: -1, T: 0, I: 0, Th: 0, N: 0, J: 0)]
+[QuantityOperation(typeof(QHalfway_L0_Mm1_T0_I0_Th0_N0_J0), typeof(QMass), typeof(QDimensionless))]
+public readonly ref partial struct QHalfway_L0_Mm1_T0_I0_Th0_N0_J0
+{
+}
+
+
+/// <summary>
 /// Calculation halfway type that represents [L^1 * M^1] dimension.
 /// This type is intended to be used only during conversion to other types,
 /// therefore it is defined as ref struct type.
@@ -17,20 +41,6 @@ namespace NetQuantities;
 [QuantityOperation(typeof(QLength), typeof(QMass), typeof(QHalfway_L1_M1_T0_I0_Th0_N0_J0))]
 [QuantityOperation(typeof(QMomentum), typeof(QTime), typeof(QHalfway_L1_M1_T0_I0_Th0_N0_J0))]
 public readonly ref partial struct QHalfway_L1_M1_T0_I0_Th0_N0_J0
-{
-}
-
-
-/// <summary>
-/// Calculation halfway type that represents [T^-2] dimension.
-/// This type is intended to be used only during conversion to other types,
-/// therefore it is defined as ref struct type.
-/// </summary>
-[Quantity(L: 0, M: 0, T: -2, I: 0, Th: 0, N: 0, J: 0)]
-[QuantityOperation(typeof(QHalfway_L0_M0_Tm2_I0_Th0_N0_J0), typeof(QTime), typeof(QFrequency))]
-[QuantityOperation(typeof(QHalfway_L0_M0_Tm2_I0_Th0_N0_J0), typeof(QLength), typeof(QAcceleration))]
-[QuantityOperation(typeof(QHalfway_L1_M1_T0_I0_Th0_N0_J0), typeof(QHalfway_L0_M0_Tm2_I0_Th0_N0_J0), typeof(QForce))]
-public readonly ref partial struct QHalfway_L0_M0_Tm2_I0_Th0_N0_J0
 {
 }
 
