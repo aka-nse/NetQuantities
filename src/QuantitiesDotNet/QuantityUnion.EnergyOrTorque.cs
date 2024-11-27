91b70f9a164c5095926c412af5de8e84ee9139aa
aka-nse <nakased.1220@gmail.com>
2024-11-27 22:41:58 +0900

    move union type of energy and torque

---
+++ src/QuantitiesDotNet/QuantityUnion.EnergyOrTorque.cs
@@ -0,0 +1,180 @@
+﻿namespace QuantitiesDotNet
+{
+    partial class QuantityUnion
+    {
+        /// <summary>
+        /// Casts into <see cref="QEnergy"/>.
+        /// </summary>
+        /// <param name="union"></param>
+        /// <returns></returns>
+        public static QEnergy AsEnergy(this QuantityUnion<QEnergy, QTorque> union)
+            => new(union.RawValue);
+
+        /// <summary>
+        /// Casts into <see cref="QTorque"/>.
+        /// </summary>
+        /// <param name="union"></param>
+        /// <returns></returns>
+        public static QTorque AsTorque(this QuantityUnion<QEnergy, QTorque> union)
+            => new(union.RawValue);
+    }
+
+    readonly partial struct QLength
+#if NET7_0_OR_GREATER
+        : IMultiplyOperators<QLength, QForce, QuantityUnion<QEnergy, QTorque>>
+#endif
+    {
+        /// <inheritdoc />
+        public static QuantityUnion<QEnergy, QTorque> operator *(QLength x, QForce y)
+            => new(x.RawValue * y.RawValue);
+    }
+
+    readonly partial struct QForce
+#if NET7_0_OR_GREATER
+        : IMultiplyOperators<QForce, QLength, QuantityUnion<QEnergy, QTorque>>
+#endif
+    {
+        /// <inheritdoc />
+        public static QuantityUnion<QEnergy, QTorque> operator *(QForce x, QLength y)
+            => new(x.RawValue * y.RawValue);
+    }
+
+    readonly partial struct QEnergy
+        : IQuantity<QEnergy>
+#if NET7_0_OR_GREATER
+        , IDivisionOperators<QEnergy, QForce, QLength>
+        , IDivisionOperators<QEnergy, QLength, QForce>
+#endif
+    {
+        /// <summary>
+        /// Casts into <see cref="QEnergy"/>.
+        /// </summary>
+        /// <param name="x"></param>
+        public static implicit operator QEnergy(QuantityUnion<QEnergy, QTorque> x)
+            => x.AsEnergy();
+
+        /// <inheritdoc />
+        public static QLength operator /(QEnergy x, QForce y)
+            => new(x.RawValue * y.RawValue);
+
+        /// <inheritdoc />
+        public static QForce operator /(QEnergy x, QLength y)
+            => new(x.RawValue * y.RawValue);
+    }
+
+    partial struct QTorque
+        : IQuantity<QTorque>
+#if NET7_0_OR_GREATER
+        , IDivisionOperators<QTorque, QForce, QLength>
+        , IDivisionOperators<QTorque, QLength, QForce>
+#endif
+    {
+        /// <summary>
+        /// Casts into <see cref="QTorque"/>.
+        /// </summary>
+        /// <param name="x"></param>
+        public static implicit operator QTorque(QuantityUnion<QEnergy, QTorque> x)
+            => x.AsTorque();
+
+        /// <inheritdoc />
+        public static QLength operator /(QTorque x, QForce y)
+            => new(x.RawValue * y.RawValue);
+
+        /// <inheritdoc />
+        public static QForce operator /(QTorque x, QLength y)
+            => new(x.RawValue * y.RawValue);
+    }
+}
+
+#if NET7_0_OR_GREATER
+namespace QuantitiesDotNet.Generic
+{
+    partial class QuantityUnion
+    {
+        /// <summary>
+        /// Casts into <see cref="QEnergy"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="union"></param>
+        /// <returns></returns>
+        public static QEnergy<T> AsEnergy<T>(this QuantityUnion<T, QEnergy<T>, QTorque<T>> union)
+            where T : INumber<T>
+            => new(union.RawValue);
+
+        /// <summary>
+        /// Casts into <see cref="QTorque"/>.
+        /// </summary>
+        /// <param name="union"></param>
+        /// <returns></returns>
+        public static QTorque<T> AsTorque<T>(this QuantityUnion<T, QEnergy<T>, QTorque<T>> union)
+            where T : INumber<T>
+            => new(union.RawValue);
+    }
+
+    readonly partial struct QLength<T>
+#if NET7_0_OR_GREATER
+        : IMultiplyOperators<QLength<T>, QForce<T>, QuantityUnion<T, QEnergy<T>, QTorque<T>>>
+#endif
+    {
+        /// <inheritdoc />
+        public static QuantityUnion<T, QEnergy<T>, QTorque<T>> operator *(QLength<T> x, QForce<T> y)
+            => new(x.RawValue * y.RawValue);
+    }
+
+    readonly partial struct QForce<T>
+#if NET7_0_OR_GREATER
+        : IMultiplyOperators<QForce<T>, QLength<T>, QuantityUnion<T, QEnergy<T>, QTorque<T>>>
+#endif
+    {
+        /// <inheritdoc />
+        public static QuantityUnion<T, QEnergy<T>, QTorque<T>> operator *(QForce<T> x, QLength<T> y)
+            => new(x.RawValue * y.RawValue);
+    }
+
+    readonly partial struct QEnergy<T>
+        : IQuantity<QEnergy<T>, T>
+#if NET7_0_OR_GREATER
+        , IDivisionOperators<QEnergy<T>, QForce<T>, QLength<T>>
+        , IDivisionOperators<QEnergy<T>, QLength<T>, QForce<T>>
+#endif
+    {
+        /// <summary>
+        /// Casts into <see cref="QEnergy"/>.
+        /// </summary>
+        /// <param name="x"></param>
+        public static implicit operator QEnergy<T>(QuantityUnion<T, QEnergy<T>, QTorque<T>> x)
+            => x.AsEnergy();
+
+        /// <inheritdoc />
+        public static QLength<T> operator /(QEnergy<T> x, QForce<T> y)
+            => new(x.RawValue * y.RawValue);
+
+        /// <inheritdoc />
+        public static QForce<T> operator /(QEnergy<T> x, QLength<T> y)
+            => new(x.RawValue * y.RawValue);
+    }
+
+    readonly partial struct QTorque<T>
+        : IQuantity<QTorque<T>, T>
+#if NET7_0_OR_GREATER
+        , IDivisionOperators<QTorque<T>, QForce<T>, QLength<T>>
+        , IDivisionOperators<QTorque<T>, QLength<T>, QForce<T>>
+#endif
+    {
+        /// <summary>
+        /// Casts into <see cref="QTorque{T}"/>.
+        /// </summary>
+        /// <param name="x"></param>
+        public static implicit operator QTorque<T>(QuantityUnion<T, QEnergy<T>, QTorque<T>> x)
+            => x.AsTorque();
+
+        /// <inheritdoc />
+        public static QLength<T> operator /(QTorque<T> x, QForce<T> y)
+            => new(x.RawValue * y.RawValue);
+
+        /// <inheritdoc />
+        public static QForce<T> operator /(QTorque<T> x, QLength<T> y)
+            => new(x.RawValue * y.RawValue);
+    }
+}
+#endif
---
+++ src/QuantitiesDotNet/QuantityUnion.cs
@@ -0,0 +1,48 @@
+﻿namespace QuantitiesDotNet
+{
+    /// <summary>
+    /// Helpers of <see cref="QuantityUnion{T1, T2}"/>.
+    /// </summary>
+    public static partial class QuantityUnion
+    {
+    }
+
+    /// <summary>
+    /// Calculation halfway type.
+    /// This type is intended to be used only during conversion to other types.
+    /// </summary>
+    public readonly struct QuantityUnion<T1, T2>
+        where T1 : IQuantity<T1>
+        where T2 : IQuantity<T2>
+    {
+        public readonly double RawValue;
+
+        internal QuantityUnion(double value) => RawValue = value;
+    }
+}
+
+#if NET7_0_OR_GREATER
+namespace QuantitiesDotNet.Generic
+{
+    /// <summary>
+    /// Helpers of <see cref="QuantityUnion{T1, T2}"/>.
+    /// </summary>
+    public static partial class QuantityUnion
+    {
+    }
+
+    /// <summary>
+    /// Calculation halfway type.
+    /// This type is intended to be used only during conversion to other types.
+    /// </summary>
+    public readonly struct QuantityUnion<TValue, T1, T2>
+        where TValue : INumber<TValue>
+        where T1 : IQuantity<T1, TValue>
+        where T2 : IQuantity<T2, TValue>
+    {
+        public readonly TValue RawValue;
+
+        internal QuantityUnion(TValue value) => RawValue = value;
+    }
+}
+#endif
